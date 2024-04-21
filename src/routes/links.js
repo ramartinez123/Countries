14870dc388e248f2db87f9536b17ea53d3631e26
Raul Martinez <rmartinezaza@gmail.com>
2024-04-21 10:39:45 -0300

    2024-04-21

--- src/routes/links.js
+++ src/routes/links.js
@@ -9,26 +9,16 @@ router.get('/add', (req,res) =>{
     res.render('links/add');
 });
 
-router.post('/add', async (req,res) =>{
-    const{country,capital,population,continent} = req.body; //req.body recive datos prop objeto
-    const newcountry ={  //guardo en un nuevo objeto
-        country,
-        capital,
-        population,
-        continent,
-        user_id: req.user.id
-    };
-   await connection.query('INSERT INTO country set?',[newcountry]);// va a tomar tiempo cuando termine sigue con otroa
-   res.redirect('/links/list');
-
-})
 
 router.get('/list', async (req,res) =>{
     const consul =await connection.query('SELECT * FROM country WHERE user_id = ?', [req.user.id]);
-    console.log(consul);
     res.render('links/list', {consul});
 });
 
+router.get('/listAll', async (req,res) =>{
+    const consulAll =await connection.query('SELECT counts.id,counts.country,counts.capital,counts.population,counts.continent FROM countries as counts LEFT JOIN country as coun ON counts.id = coun.id_country WHERE coun.user_id <> ? OR coun.user_id IS NULL', [req.user.id]);
+    res.render('links/listAll', {consulAll});
+});
 
 router.get('/delete/:id', async (req, res) => {
     const { id } = req.params;
@@ -37,7 +27,26 @@ router.get('/delete/:id', async (req, res) => {
     res.redirect('/links/list');
 });
 
-router.get('/edit/:id', async (req, res) => {
+router.get('/addCountry/:id', async (req, res) => {
+    const { id } = req.params;  
+    const consul2 = await connection.query('SELECT * FROM countries WHERE id = ?', [id]);
+    const newCountry = {
+        id_country : id,
+        country : consul2[0].country,
+        capital: consul2[0].capital,
+        population : consul2[0].population,   
+        continent : consul2[0].continent,
+        user_id: req.user.id
+    };
+    connection.query('INSERT INTO country set?',[newCountry]);
+    req.flash('success', ' Updated Successfully');
+    res.redirect('/links/list');
+
+});
+
+module.exports = router;
+
+/*router.get('/edit/:id', async (req, res) => {
     const { id } = req.params;
     const consul = await connection.query('SELECT * FROM country WHERE id = ?', [id]);
     res.render('links/edit', {consul: consul[0]});
@@ -55,6 +64,18 @@ router.post('/edit/:id', async (req, res) => {
     await connection.query('UPDATE country set ? WHERE id = ?', [newLink, id]);
     req.flash('success', ' Updated Successfully');
     res.redirect('/links/list');
-});
+});*/
 
-module.exports = router;
+/*router.post('/add', async (req,res) =>{
+    const{country,capital,population,continent} = req.body; //req.body recive datos prop objeto
+    const newcountry ={  //guardo en un nuevo objeto
+        country,
+        capital,
+        population,
+        continent,
+        user_id: req.user.id
+    };
+   await connection.query('INSERT INTO country set?',[newcountry]);// va a tomar tiempo cuando termine sigue con otroa
+   res.redirect('/links/list');
+
+})*/
